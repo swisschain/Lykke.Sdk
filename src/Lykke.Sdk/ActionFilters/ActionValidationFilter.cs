f1f26ff17724eb6ea6ce7fcdd4d5a6bd61fd30e0
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-07-03 01:07:22 +0300

    pass error response to ValidationApiException

--- src/Lykke.Sdk/ActionFilters/ActionValidationFilter.cs
+++ src/Lykke.Sdk/ActionFilters/ActionValidationFilter.cs
@@ -1,4 +1,5 @@
-﻿using Lykke.Common.ApiLibrary.Exceptions;
+﻿using Lykke.Common.ApiLibrary.Contract;
+using Lykke.Common.ApiLibrary.Exceptions;
 using Lykke.Common.ApiLibrary.Extensions;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -9,7 +10,11 @@ namespace Lykke.Sdk.ActionFilters
         public void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.ModelState.IsValid)
-                throw new ValidationApiException(context.ModelState.GetErrorMessage());
+            {
+                var errorResponse = ErrorResponseFactory.Create(context.ModelState);
+                errorResponse.ErrorMessage = context.ModelState.GetErrorMessage();
+                throw new ValidationApiException(errorResponse);
+            }
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
